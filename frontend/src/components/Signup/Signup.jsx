77548278d6b5e068306b042a9fd6d5b78a17b9db
Vivek's PC <vivekmusic19@gmail.com>
2024-02-13 15:08:01 +0530

    suc

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,11 +1,12 @@
 import { React, useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
 import axios from "axios";
 import { server } from "../../server";
 import { toast } from "react-toastify";
+
 
 const Singup = () => {
   const [email, setEmail] = useState("");
@@ -13,7 +14,7 @@ const Singup = () => {
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
   const [avatar, setAvatar] = useState(null);
-
+  const navigate=useNavigate();
   const handleFileInputChange = (e) => {
     const file=e.target.files[0]
     setAvatar(file)
@@ -34,11 +35,10 @@ const Singup = () => {
     axios
       .post(`${server}/user/create-user`, newForm,config)
       .then((res) => {
-        toast.success(res.data.message);
-        setName("");
-        setEmail("");
-        setPassword("");
-        setAvatar();
+        if(res.data.success===true){
+          navigate("/");
+        }
+
       })
       .catch((error) => {
         toast.error(error);
